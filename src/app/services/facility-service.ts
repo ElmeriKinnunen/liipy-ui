79e9e91445dfa2f0588ed6068fa9fc8b544aab78
ElmeriKinnunen <elmeri.kin@gmail.com>
2023-09-10 19:34:46 +0300

    old function removed

--- src/app/services/facility-service.ts
+++ src/app/services/facility-service.ts
@@ -12,6 +12,7 @@ import { IFacility, IfacilitiesInput, IfacilitiesResponse, IfacilityInput, Ifaci
 export class FacilityService {
   constructor(private gql: Apollo) {}
 
+  // get all facilities
   watchFacilities(variables: IfacilitiesInput): Observable<IfacilitiesResponse> {
     return this.gql
       .query<{ facilities: IfacilitiesResponse }>({
@@ -23,6 +24,7 @@ export class FacilityService {
       );
   }
 
+  // get single facility with details
   watchFacility(variables: IfacilityInput): Observable<IfacilityResponse> {
     return this.gql
       .query<{ facilityPrediction: IfacilityResponse }>({
@@ -34,75 +36,7 @@ export class FacilityService {
       );
   }
 
-  // fetchAllFacilityDetails(): Observable<IfacilityListItem[]> {
-  //   const args: IfacilitiesInput = {
-  //     statuses: ["IN_OPERATION"],
-  //     ids: [992, 990, 755, 619],
-  //     listName: "Helsinki"
-  //   };
-
-  //   const args2: IfacilitiesInput = {
-  //     statuses: ["IN_OPERATION"],
-  //     ids: [1, 2, 3, 4],
-  //     listName: "Espoo"
-  //   };
-
-  //   const args3: IfacilitiesInput = {
-  //     statuses: ["IN_OPERATION"],
-  //     ids: [10, 20, 30, 40],
-  //     listName: "Vantaa"
-  //   };
-  
-  //   return this.watchFacilities(args).pipe(
-  //     mergeMap((facilities) => {
-  //       const facilityObservables = facilities.facilities.map((facility) =>
-  //         this.watchFacility({ id: facility.id, after: 0 }).pipe(
-  //           take(1),
-  //           map((facilityData) => {
-  //             if (Array.isArray(facilityData) && facilityData.length > 0) {
-  //               facilityData = facilityData[0];
-  //             }
-  //             return {
-  //               id: facility.id,
-  //               name: facility.name.fi,
-  //               builtCapacityCar: facility.builtCapacity?.CAR,
-  //               builtCapacityElectricCar: facility.builtCapacity?.ELECTRIC_CAR || 0,
-  //               spacesAvailable: facilityData.spacesAvailable,
-  //               timeStamp: dayjs(new Date(facilityData.timestamp)).format('DD-MM-YYYY HH:mm'),
-  //             };
-  //           })
-  //         )
-  //       );
-  
-  //       return forkJoin(facilityObservables);
-  //     })
-  //   );
-  // }
-
-  fetchAllFacilityDetails(): Observable<IfacilityListItem[][]> {
-    const lists: Array<IfacilitiesInput> = [
-      {
-        statuses: ["IN_OPERATION"],
-        ids: [992, 990, 755, 619],
-        listName: "Helsinki",
-      },
-      {
-        statuses: ["IN_OPERATION"],
-        ids: [517, 1006, 303, 1233],
-        listName: "Espoo",
-      },
-      {
-        statuses: ["IN_OPERATION"],
-        ids: [1091, 751, 619, 1047],
-        listName: "Vantaa",
-      },
-    ];
-  
-    const observables = lists.map((list) => this.fetchFacilityList(list));
-  
-    return forkJoin(observables);
-  }
-
+  // get listItem
   fetchFacilityList(args: IfacilitiesInput): Observable<IfacilityListItem[]> {
     return this.watchFacilities(args).pipe(
       mergeMap((facilities) => {
@@ -128,6 +62,31 @@ export class FacilityService {
         return forkJoin(facilityObservables);
       })
     );
+  }
+
+  // get multiple lists of listItems
+  fetchAllFacilityDetails(): Observable<IfacilityListItem[][]> {
+    const lists: Array<IfacilitiesInput> = [
+      {
+        statuses: ["IN_OPERATION"],
+        ids: [992, 990, 755, 619],
+        listName: "Helsinki",
+      },
+      {
+        statuses: ["IN_OPERATION"],
+        ids: [517, 1006, 303, 1233],
+        listName: "Espoo",
+      },
+      {
+        statuses: ["IN_OPERATION"],
+        ids: [1091, 751, 619, 1047],
+        listName: "Vantaa",
+      },
+    ];
+  
+    const observables = lists.map((list) => this.fetchFacilityList(list));
+  
+    return forkJoin(observables);
   }
 
 }
